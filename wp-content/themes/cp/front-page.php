d9a66f79ff35cf3258cc420574d1a64332f78821
dmitriy.s <dmitriy.s@ideasoft.io>
2020-04-13 19:25:12 +0300

    added wp

---
+++ wp-content/themes/cp/front-page.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: Front-page
+*/
+
+ get_header(); ?>
+
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis aliquam consequuntur iste aperiam eligendi esse doloremque reiciendis deleniti necessitatibus, est excepturi ad vero distinctio, in cum asperiores facere illo! Aut!
+
+<?php get_footer(); ?>
