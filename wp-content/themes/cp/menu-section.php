fb50448008dd0d1aa4131549f2b2dc7e2b37e5d5
Nikson01 <honcharenkomykyta@gmail.com>
2020-04-25 21:04:01 +0300

    menu section almost done

---
+++ wp-content/themes/cp/menu-section.php
@@ -0,0 +1,385 @@
+<?php
+/*
+Template Name: menu-section
+*/
+ get_header(); ?>
+
+<section class="page-top page-menu">   
+    <div class="page__container page__container-menu">
+        <a class="back" href=""> <span class="left-arrow"></span> Назад</a>
+        <div class="top-nav__links page-menu__links">
+            <a href="">Главная</a>
+            <a href="">Мероприятия</a>     
+        </div>
+        <h1 class="page-title page-menu__title">
+            Фаховi заходи
+        </h1>
+    </div>
+</section>
+
+<div class="page__container page__container-menu">
+
+    <section class="page-menu__content">
+        <ul class="page-menu__content-links">
+            <li class="active-link">
+                <a href="">Все статьи</a>
+            </li>
+            <li>    
+                <svg class="icon"><use xlink:href="#line" /></svg>
+                <a href="">Форуми i конференцiї</a>
+            </li>
+            <li>
+                <svg class="icon"><use xlink:href="#line" /></svg>
+                <a href="">Круглі столи</a>
+            </li>
+            <li>
+                <svg class="icon"><use xlink:href="#line" /></svg>
+                <a href="">Засідання клубу</a>
+            </li>
+            <li>
+                <svg class="icon"><use xlink:href="#line" /></svg>
+                <a href="">Публічні лекції </a>
+            </li>
+        </ul>
+
+        <div class="page-menu__content-blocks">
+            <h2>
+                Все статьи на тему «мероприятия»
+            </h2>
+
+            <div class="page-menu__content-blocks-items">      
+
+                <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+                  <a href="#" class="flex_col-tab--1-2 flex_col-desk--1-4 main-content__block">
+                    <img src="/wp-content/themes/cp/images/img-for-article1.jpg" alt="">
+                    <div class="slider-nav__block-top yellow">
+                      <span class="nav__block-top__category"> Экспертная деятельность </span>
+                      <span class="nav__block-top__single-categ"> Заседания клуба </span>
+                    </div>
+                    <h3>
+                      Законы общественного процесса. 
+                      Концепции развития общества.
+                    </h3>
+                    <div class="slider-nav__block-bottom">
+                      <span>04.04.2020</span>
+                      <span>1 234 просмотров</span>
+                    </div>
+                  </a>
+
+            </div>
+
+            <a href="#" class="btn__more-blocks">
+                Загрузить еще
+            </a>
+        </div>
+
+    </section>
+   
+
+</div>
+
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/cp/menu-subsection.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: menu-subsection
+*/
+ get_header(); ?>
+
+
+<section class="page-top page-menu-sub">   
+    <div class="page__container">
+        <a class="back" href=""> <span class="left-arrow"></span> Назад</a>
+        <div class="top-nav__links page-menu-sub__links">
+            <a href="">Главная</a>
+            <a href="">Мероприятия</a>
+            <a href="">Собрания</a>
+        </div>
+        <h1 class="page-title page-menu-sub__title">
+            Собрания
+        </h1>
+    </div>
+</section>
+
+
+
+<?php get_footer(); ?>
